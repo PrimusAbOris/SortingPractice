008b12fe326464570b81dc501207734414a1acf2
PrimusAbOris <guy.ross.rit@gmail.com>
2023-09-09 11:54:39 -0400

    Complete!

--- src/main/FarmerAlBaryardInventoryApplicationTwo.java
+++ src/main/FarmerAlBaryardInventoryApplicationTwo.java
@@ -1,5 +1,7 @@
 package main;
 
+import java.util.Arrays;
+
 import main.animals.Pig;
 import main.service.FileService;
 
@@ -10,15 +12,35 @@ public class FarmerAlBaryardInventoryApplicationTwo {
 		FileService fileService = new FileService();
 		Pig[] pigs = fileService.getPigsFromFile();
 		String[] pigNames = new String[4];
-  		
-		// SORT the pigs by name...
 		
-		// 1. run a for loop on the pig array
-		// 2. In the for loop create an array called pig names, get the names of the pigs using
-		// the getName getter from the Pig class, save it into the pigNames string array
-		// 3. Sort the pig names
-		// 4. Print the sorted names to the console
-
+		// not in alphabetical order
+		pigNames[0] = "Corkey";
+		pigNames[1] = "Penny";
+		pigNames[2] = "Bartholomew";
+		pigNames[3] = "Rocky";
+		
+		// CANNOT SORT YET //
+		
+//		// for testing
+//		 for (Pig pig : pigs) {
+//			System.out.println(pig.getName());
+//			System.out.println(pig.getStatus());
+//		}
+//		//
+		
+		
+		for (int i = 0; i <= 3; i++) {
+			pigNames[i] = pigs[i].getName();
+		}
+		
+		// NOW WE CAN //
+		
+		Arrays.sort(pigs);
+		for (Pig pig : pigs) {
+			System.out.println(pig.getName());
+		}
+		// The Pig objects have been sorted in the manner described and
+		// their names retrieved in ascending order! Exercise complete!
 	}
 
 }
--- src/main/animals/Pig.java
+++ src/main/animals/Pig.java
@@ -1,18 +1,34 @@
 package main.animals;
 
+import java.io.Serializable;
 
-public class Pig {
+public class Pig implements Comparable<Pig>, Serializable {
+	private String name;
+	private String status;
 
-	// CREATE the field "name" which is a String
-	// CREATE the field "status" which is a String
-	
 	public Pig(String name, String status) {
-		// CREATE the constructor
+		this.name = name;
+		this.status = status;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
 	}
 	
-	
-	// Create getter and setter for name and status
-	
-	
-
+	@Override
+	public int compareTo(Pig that) {
+		return this.name.compareTo(that.name); // this will sort names in asc order
+	}
 }
